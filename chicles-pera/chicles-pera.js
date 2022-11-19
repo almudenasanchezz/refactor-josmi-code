8619967772618e0f20357a71096e0d825cc11a8a
almudenasanchezz <sanchezgallegoalmudena@gmail.com>
2022-11-19 18:26:43 +0100

    second commit

--- chicles-pera/chicles-pera.js
+++ chicles-pera/chicles-pera.js
@@ -19,6 +19,29 @@ window.addEventListener('load', () => {
 		const zipCode = document.getElementById('zip-code').value;
 		console.log(typeof zipCode);
 		let deliveryCost;
+
+		// si el número de caracteres de zipCode es distinto de 5
+		if (zipCode.length !== 5 || zipCode.startsWith('0') || isNaN(zipCode)) {
+			showBadZipFormat();
+			return;
+		}
+
+		const firstZipCodeChar = Number(zipCode.charAt(0));
+
+		console.log(typeof firstZipCodeChar);
+
+		if (zipCode.startsWith('10')) {
+			deliveryCost = 0;
+		} else if (firstZipCodeChar > 0 && firstZipCodeChar <= 5) {
+			deliveryCost = 2;
+		} else if (firstZipCodeChar > 5 && firstZipCodeChar <= 8) {
+			deliveryCost = 3;
+		} else {
+			deliveryCost = 5;
+		}
+
+		showDeliveryCost(deliveryCost);
+		showTotal(priceWithoutDeliveryCost + deliveryCost);
 	})
 });
 
